2702e14d4c543d5ab1759af7067b33e0b1d005e9
jerolezcano <144831340+jerolezcano@users.noreply.github.com>
2026-08-06 00:12:28 -0300

    Update init.ts

--- src/init.ts
+++ src/init.ts
@@ -1,11 +1,13 @@
 import { connectDB } from './db/connection';
 import app from './app';
 import { config } from './config';
+import { seedAdmin } from './utils/seedAdmin';
 
 const start = async () => {
   await connectDB();
+  await seedAdmin();   // crea el admin si no existe
   app.listen(config.port, () => {
-    console.log(`Servidor corriendo en http://localhost:${config.port}`);
+    console.log(` Servidor corriendo en http://localhost:${config.port}`);
   });
 };
 
